7d6ae5c6f36d00c3043b6abbe80c6dd1da007fc7
silici0 <silici0@gmail.com>
2022-02-11 16:57:33 -0300

    commented sandbox

--- actions/setup-aws-py/main.py
+++ actions/setup-aws-py/main.py
@@ -12,18 +12,18 @@ def main(aws_profile, root_path):
     else:
         print (f"Successfully created the directory {root_path}.aws")
 
-    
+
     DEV_TF_AWS_KEY_ID = os.environ["DEV_TF_AWS_KEY_ID"]
     DEV_TF_AWS_SECRET_KEY = os.environ["DEV_TF_AWS_SECRET_KEY"]
-    
+
     PRD_TF_AWS_KEY_ID = os.environ["PRD_TF_AWS_KEY_ID"]
-    PRD_TF_AWS_SECRET_KEY = os.environ["PRD_TF_AWS_SECRET_KEY"]    
+    PRD_TF_AWS_SECRET_KEY = os.environ["PRD_TF_AWS_SECRET_KEY"]
 
     HML_TF_AWS_KEY_ID = os.environ["HML_TF_AWS_KEY_ID"]
-    HML_TF_AWS_SECRET_KEY = os.environ["HML_TF_AWS_SECRET_KEY"]    
+    HML_TF_AWS_SECRET_KEY = os.environ["HML_TF_AWS_SECRET_KEY"]
 
-    SANDBOX_TF_AWS_KEY_ID = os.environ["SANDBOX_TF_AWS_KEY_ID"]
-    SANDBOX_TF_AWS_SECRET_KEY = os.environ["SANDBOX_TF_AWS_SECRET_KEY"]    
+    # SANDBOX_TF_AWS_KEY_ID = os.environ["SANDBOX_TF_AWS_KEY_ID"]
+    # SANDBOX_TF_AWS_SECRET_KEY = os.environ["SANDBOX_TF_AWS_SECRET_KEY"]
 
     fileAws = open(f"{root_path}.aws/credentials","w")
 
@@ -41,11 +41,11 @@ def main(aws_profile, root_path):
     fileAws.write(f"aws_access_key_id={ PRD_TF_AWS_KEY_ID }\n")
     fileAws.write(f"aws_secret_access_key={ PRD_TF_AWS_SECRET_KEY }\n\n")
 
-    fileAws.write("[sandbox]\n")
-    fileAws.write(f"aws_access_key_id={ SANDBOX_TF_AWS_KEY_ID }\n")
-    fileAws.write(f"aws_secret_access_key={ SANDBOX_TF_AWS_SECRET_KEY }\n\n")
+    # fileAws.write("[sandbox]\n")
+    # fileAws.write(f"aws_access_key_id={ SANDBOX_TF_AWS_KEY_ID }\n")
+    # fileAws.write(f"aws_secret_access_key={ SANDBOX_TF_AWS_SECRET_KEY }\n\n")
 
-    fileAws.close()    
+    fileAws.close()
 
     os.environ["AWS_PROFILE"] = aws_profile.lower()
 
